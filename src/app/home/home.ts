eb7e9e58aa3004fabc6a71753c1d8a111d825dd0
Raifa509 <fathimathulraifanp@gmail.com>
2025-11-20 17:19:08 +0400

    initial commit

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from "../header/header";
+import { Footer } from "../footer/footer";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
